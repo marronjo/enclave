bf97f51dd0418e17ef74468cb77fc87e423ba4b9
Joe Marron <marronjo@tcd.ie>
2024-09-21 09:27:35 +0800

    add encrypted records to resolver

--- src/L2Resolver.sol
+++ src/L2Resolver.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Permissioned, Permission } from "@fhenixprotocol/contracts/access/Permissioned.sol";
+import { FHE, euint256, inEuint256 } from "@fhenixprotocol/contracts/FHE.sol";
 
 // add more fields, for metadata etc.
 // need mapping string, string, string for generic fields
@@ -19,27 +21,37 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 /*
   Contract called by custom gateway to resolve id to enclave address
 */
-contract L2Resolver is Ownable {
+contract L2Resolver is Ownable, Permissioned {
+
+    mapping(string id => mapping(uint256 coinType => bytes addr)) addrRecords;
 
     mapping(string id => mapping(string key => string value)) public textRecords;
-    mapping(string id => mapping(uint256 coinType => bytes32 addr)) addrRecords;
+
+    mapping(string id => mapping(string key => euint256 encValue)) public encTextRecords;
 
     constructor() Ownable(msg.sender) {}
+
+    function setAddrRecord(string calldata id, uint256 coinType, bytes calldata addr) onlyOwner public {
+        addrRecords[id][coinType] = addr;
+    }
 
     function setTextRecord(string calldata id, string calldata key, string calldata value) onlyOwner public {
         textRecords[id][key] = value;
     }
 
-    function setAddrRecord(string calldata id, uint256 coinType, bytes32 addr) onlyOwner public {
-        addrRecords[id][coinType] = addr;
+    function setEncTextRecord(string calldata id, string calldata key, inEuint256 calldata encValue) onlyOwner public {
+        encTextRecords[id][key] = FHE.asEuint256(encValue);
+    }
+
+    function getAddrRecord(string calldata id, uint256 coinType) public view returns(bytes memory) {
+        return addrRecords[id][coinType];
     }
 
     function getTextRecord(string calldata id, string calldata key) public view returns(string memory) {
         return textRecords[id][key];
     }
 
-    function getAddrRecord(string calldata id, uint256 coinType) public view returns(bytes32) {
-        return addrRecords[id][coinType];
+    function getEncTextRecord(Permission calldata permission, string calldata id, string calldata key) onlySender(permission) public view returns(string memory) {
+        return FHE.sealoutput(encTextRecords[id][key], permission.publicKey);
     }
-
 }
